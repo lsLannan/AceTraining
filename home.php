bd82e293b9db6bdefe42a9c7b104e303c849d090
Liberty-21011806 <94843959+Liberty-21011806@users.noreply.github.com>
2023-02-27 13:43:54 +0000

    tutor homepage draft, nav bar partially linked

---
+++ home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Tutor Home</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/liberty/Lectures/css/style.css">
+    </head>
+    <body>
+        
+        <h1>Tutor Page</h1>
+        <nav>
+            <ul>
+                <li><a class="active" href="/liberty/Lectures/home.php">HOME</a></li>
+                <li><a href="/liberty/Lectures/courses_manage.php">COURSES</a></li>
+                <li><a href="/liberty/Lectures/enroll.php">ENROL</a></li>
+                <li><a href="">RECORDS</a></li>
+            </ul>
+        </nav>
+        
+        <h1>Tutor Homepage</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam nisi, accusamus consequuntur vitae sunt exercitationem porro id, fugit eum laborum nemo quia. Dolore autem architecto voluptatibus illum perspiciatis, quas nisi.</p>
+    
+        
+        <script src="" async defer></script>
+    </body>
+</html>
