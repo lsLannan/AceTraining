e7a1d59a35cc86bc13dc86ab7c3757c7aa687a7b
Liberty <94843959+Liberty-21011806@users.noreply.github.com>
2023-05-18 16:24:44 +0100

    Update rightNav.php

--- root/components/rightNav.php
+++ root/components/rightNav.php
@@ -1,6 +1,6 @@
 <div class="col c2">
 	<br><h3>Database: </h3>
-		</p><a href="functions/createDatabase.php">1 Create Database</a><br />
+		<p><a href="functions/createDatabase.php">1 Create Database</a><br />
 		<a href="functions/createUserTable.php">2 Create users Table</a><br />
 		<a href="functions/createCourseTable.php">3 Create course Table</a><br />
 		<a href="functions/createStudentTaking.php">4 Create studenttaking Table</a><br />
