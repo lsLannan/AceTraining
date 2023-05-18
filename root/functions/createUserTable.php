4caa9d82296e2fb34a7de4f50c292c37d9e908b3
Liberty <49573531+lsLannan@users.noreply.github.com>
2023-05-18 16:19:05 +0100

    userTable edit

--- root/functions/createUserTable.php
+++ root/functions/createUserTable.php
@@ -11,6 +11,7 @@ $sql = "
     CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     forename VARCHAR(50) NOT NULL, 
+    surname VARCHAR(50) NOT NULL, 
     email VARCHAR(50) NOT NULL, 
     password VARCHAR(50) NOT NULL, 
     userType VARCHAR(50) NOT NULL, 
